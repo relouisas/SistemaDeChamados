27ecd208e3311e2a4bbf596e7dd2ed62ff4ce659
Helouisa Carvalho <helouisabeatriz@gmail.com>
2018-03-08 09:08:20 -0300

    Fazendo mapeamento

--- src/Sistema/controllers/UsuariosController.java
+++ src/Sistema/controllers/UsuariosController.java
@@ -2,6 +2,10 @@ package Sistema.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import Sistema.daos.UsuarioDAO;
+import Sistema.models.Usuario;
 
 @Controller
 public class UsuariosController {
@@ -9,5 +13,20 @@ public class UsuariosController {
 	@RequestMapping ("CadastroUsuario")
 	public String Usuarios(){
 		return "CadastroUsuario";
+	}
+	
+	@RequestMapping(value="usuario", method=RequestMethod.POST)
+	public String gravar(Usuario a){
+		System.out.println("Acessando o metodo de gravar um usuario");
+		
+		System.out.println("Nome: " + a.getNome());
+		System.out.println("Email: " + a.getEmail());
+		System.out.println("Departamento: " + a.getDepartamento());
+		System.out.println("Senha:" + a.getSenha());
+		
+		UsuarioDAO dao = new UsuarioDAO();
+		dao.inserir(a);
+		
+		return "redirect:usuarios";
 	}
 }
