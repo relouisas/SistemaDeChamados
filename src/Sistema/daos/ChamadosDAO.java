691406d09461d1a5248d5cc6ff1875ad520dd22f
Helouisa Carvalho <helouisabeatriz@gmail.com>
2018-03-09 07:50:12 -0300

    Configurando Chamados

--- src/Sistema/daos/ChamadosDAO.java
+++ src/Sistema/daos/ChamadosDAO.java
@@ -1,6 +1,8 @@
 package Sistema.daos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import Sistema.models.Chamados;
 
@@ -13,9 +15,28 @@ public class ChamadosDAO {
 	}
 	
 	public boolean inserir(Chamados chamados) {
-		String sql = "insert into contatos (Texto, Status) " + "values (?, ?);";
+		String sql = "insert into chamados (Id, Nome, Email, Mensagem, Status) " + "values (?, ?, ?, ?, ?);";
 		
-		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			stmt.setLong(1, chamados.getId());
+			stmt.setString(2, chamados.getNome());
+			stmt.setString(3, chamados.getEmail());
+			stmt.setString(4, chamados.getMensagem());
+			stmt.setString(5, chamados.getStatus());
+
+			
+
+			stmt.execute();
+			stmt.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 	
 }
--- src/Sistema/models/Chamados.java
+++ src/Sistema/models/Chamados.java
@@ -3,28 +3,41 @@ package Sistema.models;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class Chamados {
-	private Long id;
-	private String Texto;
+	private Long Id;
+	private String Nome;
+	private String Email;
+	private String Mensagem;
 	private String Status;
 	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		this.Id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getMensagem() {
+		return Mensagem;
+	}
+	public void setTexto(String mensagem) {
+		Mensagem = mensagem;
+	}
 	public String getStatus() {
 		return Status;
 	}
 	public void setStatus(String status) {
 		Status = status;
 	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getTexto() {
-		return Texto;
-	}
-	public void setTexto(String texto) {
-		Texto = texto;
-	}
-	
 
 }
